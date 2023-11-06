dfa71ed6ac85345fa0b3cab1ebde4e17f59b93d1
Jayden Norlin <jnorlin2002@gmail.com>
2023-11-05 20:17:25 -0800

    Added a Sorting Example to Tester.cs

--- Scripts/Tester.cs
+++ Scripts/Tester.cs
@@ -82,6 +82,27 @@ public partial class Tester : Node
 		tabBar.TabClosePressed += TabRemoved;
 
         UpdateOpenTabs();
+
+        //Test
+        List<TaskBase> alltasks = new List<TaskBase>();
+
+        for (int i = 0; i < activeTerm.courses.Count; i++)
+        {
+            for (int j = 0; j < activeTerm.courses[i].tasks.Count; j++)
+            {
+                alltasks.Add(activeTerm.courses[i].tasks[j]);
+                //Psuedorandom date value.
+                activeTerm.courses[i].tasks[j].dueDate = DateTime.Today.AddDays(Mathf.Sin((float)i * activeTerm.courses.Count + j) * 10.0f);
+            }
+        }
+
+        List<SortExample.TaskPlaceholder> sortedTasks = SortExample.SortTasks(alltasks.ToArray());
+
+        for (int i = 0; i < sortedTasks.Count; i++)
+        {
+            int j = sortedTasks[i].originalIndex;
+            GD.Print(((PrimaryTask)alltasks[j]).taskName + " date " + ((PrimaryTask)alltasks[j]).dueDate.ToString());
+        }
     }
 
     #region Initialization
@@ -210,4 +231,90 @@ public partial class Tester : Node
 
 		taskDescriptionArea.Text = task.taskDescription;
 	}
+}
+
+public class SortExample
+{
+    //Our application will be sorting a list of Tasks based on a variety of factors.
+    //As such, this struct is used as a placeholder.
+    //The "value" variable will be set based on what factor we want to sort by (due date, assignment weight, expected assignment length, etc).
+    public struct TaskPlaceholder
+    {
+        public int originalIndex;
+        public double value;
+    }
+
+    public static List<TaskPlaceholder> SortTasks(TaskBase[] unsortedTasks)
+    {
+        List<TaskPlaceholder> unsortedList = new List<TaskPlaceholder>(unsortedTasks.Length);
+
+        for (int i = 0; i < unsortedTasks.Length; i++)
+        {
+            TaskPlaceholder t = new TaskPlaceholder();
+
+            t.originalIndex = i;
+
+            //Here the "value" would be set based on the factor we wanted to sort by. 
+            //Setting it to the Due Date for the demonstration.
+            t.value = unsortedTasks[i].dueDate.ToOADate();
+
+            unsortedList.Add(t);
+        }
+
+        //Heapify
+        for (int i = Mathf.CeilToInt(unsortedList.Count / 2); i >= 0; i--)
+        {
+            MaxHeapNode(i, unsortedList);
+        }
+
+        List<TaskPlaceholder> sortedList = new List<TaskPlaceholder>();
+
+        //"Deheapify"
+        //Linearly arrange all elements of the heap.
+        while(unsortedList.Count != 0)
+        {
+            sortedList.Add(unsortedList[0]);
+
+            Swap(0, unsortedList.Count - 1, unsortedList);
+            unsortedList.RemoveAt(unsortedList.Count - 1);
+
+            MaxHeapNode(0, unsortedList);
+        }
+
+        return sortedList;
+    }
+
+    //Referred to as "Heapify" in some places.
+    static void MaxHeapNode(int node, List<TaskPlaceholder> heap)
+    {
+        int largestNode = -1;
+        //Check Left Child.
+        if ((node * 2) + 1 < heap.Count && heap[(node * 2) + 1].value > heap[node].value)
+        {
+            largestNode = (node * 2) + 1;
+        }
+        else
+        {
+            largestNode = node;
+        }
+        //Check Right Node.
+        if ((node * 2) + 2 < heap.Count && heap[(node * 2) + 2].value > heap[largestNode].value)
+        {
+            largestNode = (node * 2) + 2;
+        }
+        //Swap current node with largest node, whether that is the left or right one.
+        if (largestNode != node)
+        {
+            Swap(node, largestNode, heap);
+            //If the node is swapped, echo the process down the tree.
+            MaxHeapNode(largestNode, heap);
+        }
+    }
+
+    static void Swap(int a, int b, List<TaskPlaceholder> heap)
+    {
+        TaskPlaceholder hold = heap[a];
+        heap[a] = heap[b];
+        heap[b] = hold;
+    }
 }
